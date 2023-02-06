02ae154a4b51e263c8629a8a6bc647ba26a3d6a2
Laurine Denis <laurinedenis@MacBook-Pro-de-Laurine.local>
2023-02-06 16:15:04 +0100

    test push pour cerise

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -38,7 +38,7 @@ function App() {
 
   if (!verif && persistantReducer.userReducer.user)
     {
-        console.log("Check reco front", persistantReducer.userReducer.user?.username);
+        console.log("Check reco front 2", persistantReducer.userReducer.user?.username);
         utils.gameSocket.emit('CHECK_RECONNEXION', persistantReducer.userReducer.user ? persistantReducer.userReducer.user : '');
         setVerif(true)
     }
