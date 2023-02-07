5df2d302f5f90019d1c099c8a8e7912822ccdb67
Maxime Rochet <mrochet@z3r2p1.42lyon.fr>
2023-02-07 20:05:59 +0100

    design_profil

--- frontend/src/pages/signup/Signup.tsx
+++ frontend/src/pages/signup/Signup.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { ip } from "../../App";
 import Cookies from "universal-cookie";
 import { setUser } from "../../state/action-creators";
+import "./signup.scss";
 
 const cookies = new Cookies();
 const jwt = cookies.get('jwt');
@@ -60,21 +61,24 @@ const Signup = () => {
 
   return (
 
-    <button
-      className="i42-button"
-      onClick={() =>
-        window.open(
-          `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-2ba494ca541577ab12aead4ea4f59fc22b4c2bea05058775f2524344f2e602a9&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fhome&response_type=code`,
-          "_self"
-        )
-      }
-    >
-      <img
-        className="i42-logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/42_Logo.svg/langfr-280px-42_Logo.svg.png"
-        alt=""
-      />
-    </button>
+    <div className="div_signup">
+      <button
+        className="i42_button"
+        onClick={() =>
+          window.open(
+            `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-2ba494ca541577ab12aead4ea4f59fc22b4c2bea05058775f2524344f2e602a9&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fhome&response_type=code`,
+            "_self"
+          )
+        }
+      >
+        <img
+          className="i42_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/42_Logo.svg/langfr-280px-42_Logo.svg.png"
+          alt=""
+        />
+      </button>
+      <h1>Connect with 42</h1>
+    </div>
   );
 };
 
