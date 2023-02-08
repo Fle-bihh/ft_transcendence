64b9f69ab650fa7955b23348e84198fcb73565e1
Laurine Denis <laurinedenis@MacBook-Pro-de-Laurine.local>
2023-02-08 23:24:10 +0100

    pong pong

--- frontend/src/components/NotifInterceptor/NotifInterceptor.tsx
+++ frontend/src/components/NotifInterceptor/NotifInterceptor.tsx
@@ -25,7 +25,7 @@ export default function NotifInterceptor(props: { children: any }) {
     if (userReducer.user)
     {
       utilsReducer.socket.emit("STORE_CLIENT_INFO", { user: userReducer.user });
-      utilsReducer.gameSocket.emit("CHECK_RECONNEXION", {username : userReducer.user?.username});
+      // utilsReducer.gameSocket.emit("CHECK_RECONNEXION", {username : userReducer.user?.username});
     }
   });
 
